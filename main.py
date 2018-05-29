f99a301263059b18925022c0f27f31dee764a7aa
Anuvind Bhat <anuvind.bhat@gmail.com>
2018-05-29 18:26:24 +0530

    added code to get absolute url

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 
 # BASE_URL = 'https://phet.colorado.edu/en/simulations'
@@ -7,5 +8,10 @@ BASE_URL = 'http://doer.metastudio.org/phet/en/simulations.html'
 page = requests.get(BASE_URL)
 soup = BeautifulSoup(page.content, 'lxml')
 
-for tag in soup.select('span.simulation-list-title'):
-	print(tag.get_text())
+names = soup.select('span.simulation-list-title')
+links = soup.select('a.simulation-link')
+
+for tag, link in zip(names, links):
+	print('{}: {}'.format(tag.string, link.get('href')))
+	print(urljoin(BASE_URL, link.get('href')))
+	print()
